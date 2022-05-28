2573e30c4ce083b598f7fc97a1cd7488e8caec29
Joshua Wu <jww2145@columbia.edu>
2022-05-27 21:57:32 -0400

    commit tree 33f31cb25f7c82a8d9472273780ab1fc3478bbea

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, events){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return messages;
+}
+
+function countDown(int){
+    let i = int;
+    while(i >= 0){
+        console.log(i)
+        i--
+    }
+}
